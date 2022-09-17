6c246193603fb5c159cce0519c2baf2439d26185
Vadym Chutur <falloutgreck@gmail.com>
2022-09-18 01:08:05 +0300

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,38 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const imagTumbInGalery = document.querySelector('.gallery');
+imagTumbInGalery.addEventListener('click', openOriginalImgAtClickPrev);
+imagTumbInGalery.innerHTML = createGalleryItemsMarkup(galleryItems);
+
+function createGalleryItemsMarkup(items) {
+  return items
+    .map(
+      ({ preview, original, description }) =>
+        `<div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="large-image.jpg"
+        alt="${description}"
+      />
+    </a>
+  </div>`
+    )
+    .join('');
+}
+
+function openOriginalImgAtClickPrev(evt) {
+  evt.preventDefault();
+  console.log(evt.currentTarget);
+  const giveLinkToOriginalImg = document.querySelector('.gallery__link');
+  console.log(giveLinkToOriginalImg);
+  //   if (evt.target.nodeName !== IMG) {
+  //     return;
+  //   }
+
+  //   console.log(selectedImg);
+}
