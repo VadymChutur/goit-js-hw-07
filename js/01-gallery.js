b628a3adfb6eefdb7ddd34019c75f582c9152adf
Vadym Chutur <falloutgreck@gmail.com>
2022-09-18 16:07:35 +0300

    update 01 gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,42 +1,54 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-// console.log(galleryItems);
-
 const imagTumbInGalery = document.querySelector('.gallery');
-imagTumbInGalery.addEventListener('click', openOriginalImgAtClickPrev);
 imagTumbInGalery.innerHTML = createGalleryItemsMarkup(galleryItems);
+imagTumbInGalery.addEventListener('click', openOriginalImgAtClickPrev);
 
 function createGalleryItemsMarkup(items) {
   return items
     .map(
       ({ preview, original, description }) =>
         `<div class="gallery__item">
-    <a class="gallery__link" href="${original}">
-      <img
+        <a class="gallery__link" href="${original}">
+        <img
         class="gallery__image"
         src="${preview}"
         data-source="large-image.jpg"
         alt="${description}"
-      />
-    </a>
-  </div>`
+        />
+        </a>
+        </div>`
     )
     .join('');
 }
 
 function openOriginalImgAtClickPrev(evt) {
   evt.preventDefault();
-  console.log(evt.currentTarget);
-  const giveLinkToOriginalImg =
-    evt.currentTarget.querySelector('.gallery__link');
-  console.log(giveLinkToOriginalImg);
-  console.log(evt.currentTarget.nodeName);
-  console.log(evt.target.nodeName);
+  const giveLinkToOriginalImg = evt.target.parentNode;
 
-  //   if (evt.target.nodeName !== IMG) {
-  //     return;
-  //   }
+  if (evt.target.nodeName !== 'IMG') {
+    return;
+  }
 
-  //   console.log(selectedImg);
+  const instance = basicLightbox.create(
+    `
+    <img src="${giveLinkToOriginalImg.href}" width="800" height="600">
+    `,
+    {
+      onShow: (instance) => {
+        document.addEventListener('keydown', closeKeyDownKeyEsc);
+      },
+      onClose: (instance) => {
+        document.removeEventListener('keydown', closeKeyDownKeyEsc);
+      },
+    }
+  );
+
+  function closeKeyDownKeyEsc(evt) {
+    if (evt.code === 'Escape') {
+      instance.close();
+    }
+  }
+  instance.show();
 }
